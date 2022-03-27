64e4adbe06a5bf2bb642737eaccdc3c3d734c13d
samy.nalbandian <samy.nalbandian@epita.fr>
2022-03-27 16:32:25 +0200

    add script to launch stack and started to write a makefile

--- launch-app-local.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-if [[ 'sudo lsof -ti:5432' ]] # check if db is up locally
-then
-docker-compose up -d app-dev --env-file .env.local
-else
-docker-compose up -d app-dev postgres --env-file .env.local
-fi
